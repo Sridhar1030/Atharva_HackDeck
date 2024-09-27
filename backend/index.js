0c98066242c666a221b1de3b2b1dbcc111dddcb3
Sridhar1030 <sridharpillai75@gmail.com>
2024-09-27 15:34:22 +0530

    VoterId

--- backend/index.js
+++ backend/index.js
@@ -8,7 +8,7 @@ const app = express();
 //Routes
 
 //Server Run
-const PORT = process.env.PORT || 8001;
+const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`)
 })

3b3b5b1dc549cae4bc59603b0cfb56f2da8ae3eb
Sridhar1030 <sridharpillai75@gmail.com>
2024-09-28 12:49:18 +0530

    .

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -1,5 +1,5 @@
 import {Router} from 'express';
-import { getUserLocation, loginUser } from '../controllers/user.controller.js';
+import { getUserInfo, getUserLocation, loginUser } from '../controllers/user.controller.js';
 
 
 
@@ -7,6 +7,7 @@ const userRouter = Router();
 
 userRouter.route("/login").post(loginUser)
 userRouter.route("/location/:_id").get(getUserLocation);
+userRouter.route("/:_id").get(getUserInfo)
 
 
 export {userRouter}

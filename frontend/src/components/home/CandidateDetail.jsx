ecf5e41aa4adda47b3010f413dd3d6c5dddee7e1
omkarlaad <omkarlaad98@gmail.com>
2024-09-28 14:59:09 +0530

    Navbar done

--- frontend/src/components/home/CandidateDetail.jsx
+++ frontend/src/components/home/CandidateDetail.jsx
@@ -19,12 +19,33 @@ const CandidateDetail = () => {
                         <div className="flex space-x-4 mt-4 md:mt-0">
                             {/* Candidate Image */}
                             <div className="w-28 h-28 md:w-28 md:h-28 rounded-full overflow-hidden border border-gray-300">
-                                <img
-                                    src={candidate.candidate_image}
-                                    alt={candidate.name}
-                                    className="w-full h-full object-fit"
-                                />
+                                {candidate.candidate_image ? (
+                                    <img
+                                        src={candidate.candidate_image}
+                                        alt={candidate.name}
+                                        className="w-full h-full object-cover text-sm" // Change object-fit to object-cover for better fitting
+                                    />
+                                ) : (
+                                    <svg
+                                        xmlns="http://www.w3.org/2000/svg"
+                                        width="24"
+                                        height="24"
+                                        viewBox="0 0 24 24"
+                                        fill="none"
+                                        stroke="currentColor"
+                                        strokeWidth="2"
+                                        strokeLinecap="round"
+                                        strokeLinejoin="round"
+                                        className="w-full h-full"
+                                    >
+                                        <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                                        <path d="M12 12m-9 0a9 9 0 1 0 18 0a9 9 0 1 0 -18 0" />
+                                        <path d="M12 10m-3 0a3 3 0 1 0 6 0a3 3 0 1 0 -6 0" />
+                                        <path d="M6.168 18.849a4 4 0 0 1 3.832 -2.849h4a4 4 0 0 1 3.834 2.855" />
+                                    </svg>
+                                )}
                             </div>
+
                             {/* Party Logo */}
                             <div className="w-28 h-28 md:w-28 md:h-28 rounded-full overflow-hidden border border-gray-300">
                                 <img
